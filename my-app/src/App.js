4075891a26af442ad14eba38cb3451c793fde233
sbojorge <sara.bojorge@gmail.com>
2023-05-08 10:09:53 +0200

    add a state to the component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ import StatefulGreeting from './components/StatefulGreeting';
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I'm a statefulGreeting class component"/>
+      <StatefulGreeting greeting="I'm a statefulGreeting class component" name="Gati"/>
     </div>
   );
 }
