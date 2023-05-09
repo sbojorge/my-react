f7b9395edbddfff63236df405dc61852cd02cdbe
sbojorge <sara.bojorge@gmail.com>
2023-05-09 21:30:58 +0200

    update parent from child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,13 @@
 
 import './App.css';
 
-import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
-  return (
-    <div className="App">
-      
-        <NestingComponents />
-    </div>
-  )  
-  
-  }
+  return <div className="App">
+      <MethodsAsPropsParent />
+  </div>;
+}
 
 
 export default App;
