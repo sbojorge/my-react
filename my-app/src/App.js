9abeace4f7e802f12dad1d98295f507a0429586e
sbojorge <sara.bojorge@gmail.com>
2023-05-09 19:59:59 +0200

    use nested components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 
 import './App.css';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+
+import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={false}/>
+      
+        <NestingComponents />
     </div>
-  );
-}
+  )  
+  
+  }
+
 
 export default App;
