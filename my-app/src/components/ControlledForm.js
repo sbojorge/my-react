9fc3badd555bf6269b0cefcf538bef26e6f7334f
sbojorge <sara.bojorge@gmail.com>
2023-05-16 12:29:41 +0200

    add select and textarea fields in the form and give React control over them

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -4,7 +4,9 @@ export class ControlledForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: ''
+            name: '',
+            category: 'Website issue',
+            comment: ''
         }
     }
     handleChangeName = (event) => {
@@ -12,14 +14,38 @@ export class ControlledForm extends Component {
             name: event.target.value
         })
     }
+    pickCategory = (event) => {
+        this.setState({
+            category: event.target.value
+        })
+    }
+    addComment = (event) => {
+        this.setState({
+            comment: event.target.value
+        })
+    }
     render() {
         return (
             <div>
+                <h2>Please fill out the form below:</h2>
                 <form>
-                    <label htmlFor='id-name'>Your name:</label>
-                    <input onChange={this.handleChangeName} value={this.state.name} type='text' id='id-name' name='name' />
+                    <div>
+                        <label htmlFor='id-name'>Your name:</label>
+                        <input onChange={this.handleChangeName} value={this.state.name} type='text' id='id-name' name='name' />
+                    </div>
+                    <div>
+                        <label htmlFor='id-category'>Query category:</label>
+                        <select  onChange={this.pickCategory} value={this.state.category} id='id-category' name='category'>
+                            <option value='website'>Website issue</option>
+                            <option value='order'>Order issue</option>
+                            <option value='general'>General inquiry</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor='id-comments'>Comments:</label>
+                        <textarea value={this.state.comment} onChange={this.addComment} id='id-comments' name='comments' />
+                    </div>
                     <input type='submit' value='Submit' />
-
                 </form>
             </div>
         )
