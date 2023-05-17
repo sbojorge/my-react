2d607fa719a4955877cb7f5491824301373c5309
sbojorge <sara.bojorge@gmail.com>
2023-05-17 17:59:33 +0200

    use hooks with objects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 
 function App() {
   return <div className="App">
-      <UseStateWithArrays />
+      <UseStateWithObjects />
   
   </div>;
 }
