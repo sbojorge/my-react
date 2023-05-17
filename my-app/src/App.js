736ab78a91dac033f01123d0c70a3a5fb824e190
sbojorge <sara.bojorge@gmail.com>
2023-05-17 19:25:56 +0200

    use useEffect in a functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import UseStateWithObjects from './components/UseStateWithObjects';
+
+import UseEffectCounter from './components/UseEffectCounter';
 
 
 function App() {
   return <div className="App">
-      <UseStateWithObjects />
+      
+      <UseEffectCounter />
   
   </div>;
 }
