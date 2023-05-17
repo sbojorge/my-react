63e6303b5c3a8f3fe8489c5eacffd38d450c4375
sbojorge <sara.bojorge@gmail.com>
2023-05-17 17:16:14 +0200

    use hooks with arrays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithArrays from './components/UseStateWithArrays';
 
 
 function App() {
   return <div className="App">
-      <ControlledFormHooks />
+      <UseStateWithArrays />
   
   </div>;
 }
