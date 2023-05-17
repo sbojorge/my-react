1dc55322d911a4ccc67fff7da98d5d896ef954f7
sbojorge <sara.bojorge@gmail.com>
2023-05-17 22:00:52 +0200

    useEffect to cleanup functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 
-import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 
 
 function App() {
   return <div className="App">
       
-      <UseEffectCounter />
+      <UseEffectCounterContainer />
   
   </div>;
 }
--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,10 +2,23 @@ import React, { useState, useEffect } from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+    
     useEffect(() => {
-        
+        console.log('count 1 effect');
         document.title = count;
     }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            console.log('interval executed')
+            setTime(time => time + 1)
+        }, 1000);
+        return () => {
+            console.log('cleaning up!')
+            clearInterval(interval);
+        }
+    }, []);
     return (
         <div>
             <button onClick={
@@ -13,6 +26,7 @@ function UseEffectCounter() {
             }>
                 Increment count({count})
             </button>
+            <h2>Time is {time}</h2>
         </div>
     )
 }
