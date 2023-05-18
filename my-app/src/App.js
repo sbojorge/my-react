96abfe2369715e790a9a72c16726bca9a00d3030
sbojorge <sara.bojorge@gmail.com>
2023-05-18 23:25:04 +0200

    use the POST method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-import HTTPRequests from './components/HTTPRequests';
+import HTTPPost from './components/HTTPPost';
+
 
 
 function App() {
   return <div className="App">
       
-      <HTTPRequests />
+      <HTTPPost />
   
   </div>;
 }
