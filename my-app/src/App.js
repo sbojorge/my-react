e51982990f3fdae486a26f910bffae1206f59d84
sbojorge <sara.bojorge@gmail.com>
2023-05-18 17:37:29 +0200

    fetch data from an API using axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
-
-import UseEffectCounterContainer from './components/UseEffectCounterContainer';
+import HTTPRequests from './components/HTTPRequests';
 
 
 function App() {
   return <div className="App">
       
-      <UseEffectCounterContainer />
+      <HTTPRequests />
   
   </div>;
 }
